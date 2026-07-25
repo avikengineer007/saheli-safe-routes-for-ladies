7c3aa58375f138e4f7dce3af0a3155c9f18b707c
avikengineer007 <avikghosh072004@gmail.com>
2026-07-25 13:35:45 +0900

    style: update Leaflet map fallback to sleek CartoDB Dark Matter tiles

--- mobile/src/components/MapViewCanvas.tsx
+++ mobile/src/components/MapViewCanvas.tsx
@@ -46,8 +46,8 @@ export const MapViewCanvas: React.FC<MapViewCanvasProps> = ({
         zoomControl: true
       });
 
-      // CartoDB Voyager OpenStreetMap Tiles (Ultra-clear high-contrast road map)
-      Leaflet.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+      // CartoDB Dark Matter OpenStreetMap Tiles (Sleek dark-mode vector map aesthetic)
+      Leaflet.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 19
